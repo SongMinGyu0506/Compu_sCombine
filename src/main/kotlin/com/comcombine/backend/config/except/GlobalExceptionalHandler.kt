5aaecc696b90e64743f8e64f6f22ced86014ee79
SongMinGyu <ggp04114@naver.com>
2023-06-30 16:31:41 +0900

    feat: 예외처리 코드 개선 및 추가

--- src/main/kotlin/com/comcombine/backend/config/except/GlobalExceptionalHandler.kt
+++ src/main/kotlin/com/comcombine/backend/config/except/GlobalExceptionalHandler.kt
@@ -10,27 +10,32 @@ import org.springframework.web.context.request.WebRequest
 @RestControllerAdvice
 class GlobalExceptionalHandler {
 
+    private fun exceptionWrapper(ex: Exception, request:WebRequest, httpStatus: HttpStatus):ResponseEntity<*> {
+        ex.printStackTrace()
+        return ResponseEntity(ExceptionWrapper.makeResponse(ex,request,httpStatus),httpStatus)
+    }
     @ExceptionHandler(Exception::class)
     fun exceptionHandle(ex:Exception, request:WebRequest):ResponseEntity<*> {
-        ex.printStackTrace()
-        return ResponseEntity(ExceptionWrapper.makeResponse(ex,request, HttpStatus.INTERNAL_SERVER_ERROR),HttpStatus.INTERNAL_SERVER_ERROR)
+        return exceptionWrapper(ex, request, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(CrawlingException::class)
     fun crawlException(ex:Exception, request: WebRequest):ResponseEntity<*> {
-        ex.printStackTrace()
-        return ResponseEntity(ExceptionWrapper.makeResponse(ex,request, HttpStatus.INTERNAL_SERVER_ERROR),HttpStatus.INTERNAL_SERVER_ERROR)
+        return exceptionWrapper(ex, request, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(JwtCustomException::class)
     fun jwtException(ex:Exception, request: WebRequest):ResponseEntity<*> {
-        ex.printStackTrace()
-        return ResponseEntity(ExceptionWrapper.makeResponse(ex,request,HttpStatus.UNAUTHORIZED),HttpStatus.UNAUTHORIZED)
+        return exceptionWrapper(ex, request, HttpStatus.UNAUTHORIZED)
     }
 
     @ExceptionHandler(UserConflictException::class)
     fun userConflictException(ex:Exception, request: WebRequest):ResponseEntity<*> {
-        ex.printStackTrace()
-        return ResponseEntity(ExceptionWrapper.makeResponse(ex,request,HttpStatus.CONFLICT),HttpStatus.CONFLICT)
+        return exceptionWrapper(ex, request, HttpStatus.CONFLICT)
+    }
+
+    @ExceptionHandler(UserNotFoundException::class)
+    fun userNotFoundException(ex:Exception, request: WebRequest):ResponseEntity<*> {
+        return exceptionWrapper(ex,request,HttpStatus.BAD_REQUEST)
     }
 }
